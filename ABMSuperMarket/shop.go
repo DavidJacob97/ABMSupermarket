d281fe5eaa641165b5c1b3bb13dcf7d504920c72
17207738 <37069315+17207738@users.noreply.github.com>
2020-11-22 19:13:24 +0000

    Update shop.go
    some updates .Now the code runs non stop as we wanted it though I am unable to view results of whats printed out that way I have checked it by simulation 1 day only and works alright .I commented out the Tills section on line nr 121-130 as it gave error.

--- ABMSuperMarket/shop.go
+++ ABMSuperMarket/shop.go
@@ -7,20 +7,28 @@ import (
 )
 
 var timeofday int
-
+var maxcapacity int
 var handsanitizerremaining int
-
+var DaysRemaining int
 func timeloop() {
 
 	timeofday = timeofday + 1
+
+	if timeofday == 1440 {
+
+		timeofday=0
+		DaysRemaining=DaysRemaining-1
+
+	
+    }
 }
-func setCovid() int {
+func setCovid() {
 	rand.Seed(time.Now().UnixNano())
 	min := 1
 	max := 5
 	covidlv := rand.Intn((max - min + 1) + min)
 
-	var maxcapacity int
+
 	if covidlv == 0 {
 
 		maxcapacity = 100
@@ -53,19 +61,38 @@ func setCovid() int {
 
 	}
 
-	return maxcapacity
+	
 
 }
 
 func openshop() {
 
-	fmt.Println("Tills opening")
 
+     
+	fmt.Println("Tills opening")
+	
+	if DaysRemaining==0{
+	    setCovid()
+	    
+	    DaysRemaining=7
+	}
+	
+	for timeofday < 1320 && timeofday >= 540 {
+
+		customer()
+		handsanitizer()
+		timeloop()
+
+	}
+   if timeofday == 1320 {
+
+		closeshop()
+	}
 }
 
 func customer() {
 
-	fmt.Println("customers incoming")
+	
 	handsanitizerremaining = handsanitizerremaining - 1
 }
 
@@ -75,18 +102,31 @@ func closeshop() {
 	fmt.Println("processremaining customers")
 	fmt.Println("close all tills")
 	fmt.Println("close shop")
+	
+		for timeofday >= 1320 || timeofday < 540 {
 
-}
-
-func closeAllTills() {
-	for i := 0; i < len(allTills); i++ {
-		allTills[i].isOpen = false
+		fmt.Println("shop is closed")
+		
+      timeloop()
+	
 	}
+	
+	 if timeofday == 540 {
+
+	openshop()
+	}
+	
 }
 
-func getTills() []Till {
-	return allTills
-}
+//func closeAllTills() {
+//	for i := 0; i < len(allTills); i++ {
+//		allTills[i].isOpen = false
+//	}
+//}
+
+//func getTills() []Till {
+//	return allTills
+//}
 
 func handsanitizer() {
 
@@ -103,29 +143,103 @@ func main() {
 
 	handsanitizerremaining = 100
 
-	maxcapacity := setCovid()
 
 	fmt.Println(maxcapacity)
+    openshop()
 
-	if timeofday == 540 {
-
-		openshop()
-	}
-	if timeofday == 1320 {
-
-		closeshop()
-	}
-
-	for timeofday < 1320 && timeofday >= 540 {
-
-		customer()
-		handsanitizer()
-		timeloop()
-
-	}
-	for timeofday > 1320 && timeofday < 540 {
-
-		fmt.Println("shop is closed")
-
-	}
 }
+
+type Queue struct {
+	itemProcessingTime int
+	inQueue            []Customer
+}
+
+type Till struct {
+	name        string
+	isFastTrack bool
+	isOpen      bool
+	queue       Queue
+}
+
+func openTill(t Till) {
+	t.isOpen = true
+}
+
+func closeTill(t Till) {
+	t.isOpen = false
+}
+
+/*
+func getAvgQueueNum() int {
+	sum := 0
+	for i := 0; i < len(allTills); i++ {
+		sum += allTills[i].queueLength
+	}
+	avgQueueLength := sum / len(allTills)
+	return avgQueueLength
+}
+*/
+
+func newQueue(itemProcessingTime int) *Queue {
+	q := Queue{}
+	q.itemProcessingTime = itemProcessingTime
+	return &q
+}
+
+func newTill(name string, isFastTrack bool, isOpen bool, itemProcessingTime int) *Till {
+	t := Till{name: name}
+	t.isFastTrack = isFastTrack
+	t.isOpen = isOpen
+	t.queue = *newQueue(itemProcessingTime)
+	return &t
+}
+
+func remove(slice []Customer, i int) []Customer {
+	return append(slice[:i], slice[i+1:]...)
+}
+
+func processCustomer(till Till) {
+	if !till.isOpen {
+		fmt.Printf("Till %s is currently closed\n", till.name)
+		return
+	}
+
+	if len(till.queue.inQueue) == 0 {
+		fmt.Printf("No customers currently in queue at till %s\n", till.name)
+		return
+	}
+
+	//process the first customer in queue
+	fmt.Printf("Processing customer %s at till %s\nNumber of items: %d\n", till.queue.inQueue[0].name, till.name, till.queue.inQueue[0].items)
+
+	for i := till.queue.inQueue[0].items; i != 0; i-- {
+		time.Sleep(time.Duration(till.queue.itemProcessingTime) * time.Second)
+		fmt.Printf("Processed item %d\n", i)
+	}
+
+	fmt.Printf("Customer %s has checked out\n", till.queue.inQueue[0].name)
+
+	//remove first element in customer slice from queue and maintain order
+	till.queue.inQueue = remove(till.queue.inQueue, 0)
+}
+type Customer struct {
+	name        string
+	patience    int
+	isAntiMask  bool
+	items       int
+	queueNumber int
+}
+
+func addCustomer(setItems int, setQueueNumber int) *Customer {
+	temp := Customer{items: setItems}
+	temp.queueNumber = setQueueNumber
+
+	return &temp
+}
+
+func newCustomer() {
+	//customerSlice := make([]Customer, 20)
+
+}
+
+
