6f32c167432897d88e559602c45c6ce8a1913415
Rsentinell <26678372+Rsentinell@users.noreply.github.com>
2020-11-22 14:34:47 +0100

    Rsentinell

--- ABMSuperMarket/Customer.go
+++ ABMSuperMarket/Customer.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+   "fmt"
+   "time"
+)
+
 // Customer is a person buying food
-type Customer struct {
-	patience   int
-	isAntiMask bool
+type customer struct {
 	items      int
+	queNumber  int
+}
+
+func addCustomer (setItems int, setQueNumber int) *customer {
+	temp := customer{items: setItems}
+	temp.queNumber = setQueNumber
+
+	return temp
+}
+
+func main() {
+	customerSlice := make([]customer, 20)
+
+
 }
