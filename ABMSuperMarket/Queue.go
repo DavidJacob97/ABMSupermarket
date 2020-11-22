a63b27109886d071de432bbf9672ecae284cd8b7
Mats <eple6@msn.com>
2020-11-22 12:37:36 +0000

    Queue

---
+++ ABMSuperMarket/Queue.go
@@ -0,0 +1,12 @@
+package main
+
+type Queue struct {
+	count              int
+	itemProcessingTime int
+	till               Till
+	customers          []Customer
+}
+
+func processCustomer(Customer) {
+
+}
