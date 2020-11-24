567c67f020f559958a2361c2ebc64c9c22c8e4fe
17207738 <37069315+17207738@users.noreply.github.com>
2020-11-24 20:17:16 +0000

    Update shop.go
    generating customers while the shop is opened and the maximum capacity hasn't been reached

--- ABMSuperMarket/shop.go
+++ ABMSuperMarket/shop.go
@@ -216,7 +216,7 @@ func generateCustomers() {
 	for   {
 		rand.Seed(time.Now().UnixNano())
 		
-		   if shop.customerInstore<shop.maxCapacity{
+		   if shop.customerInstore<shop.maxCapacity && shop.shopOpened==true{
 		
 		r := rand.Intn(len(foreNames))
 		foreName := foreNames[r]
