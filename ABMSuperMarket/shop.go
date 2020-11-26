8205551d8c724bdd8d483fe348e9cfb5c9921bb1
DavidJacob97 <david086.jacob@gmail.com>
2020-11-26 19:16:43 +0000

    small edit

--- ABMSuperMarket/shop.go
+++ ABMSuperMarket/shop.go
@@ -75,7 +75,7 @@ func addCustomerToShop() {
 				arrivingCustomers[len(arrivingCustomers)-1] = e
 				arrivingCustomers = arrivingCustomers[:len(arrivingCustomers)-1]
 
-				shop.handSanitizerRemaining -= 1
+				shop.handSanitizerRemaining--
 			} else {
 				fmt.Printf("Customer %s does not have a mask and was refused entry\n", arrivingCustomers[randNum].name)
 				copy(arrivingCustomers[randNum:], arrivingCustomers[randNum+1:])
